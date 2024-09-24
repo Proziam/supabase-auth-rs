94ab8cf375122a4925988752976d3f8065493f58
Eric Biggs <eric@proziam.com>
2024-09-24 08:29:06 -0400

    Cleanup uneccessary steps in tests

--- tests/client_tests.rs
+++ tests/client_tests.rs
@@ -1,6 +1,5 @@
 use std::{collections::HashMap, env};
 
-use reqwest::header;
 use supabase_auth::models::{
     AuthClient, DesktopResendParams, ResendParams, SignInWithOAuthOptions, UpdateUserPayload,
 };
@@ -20,15 +19,11 @@ async fn create_client_test_valid() {
 async fn sign_in_with_password_test_valid() {
     let auth_client = create_test_client();
 
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
     let demo_password = "qwerqwer";
 
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
-
     let session = auth_client
-        .sign_in_with_email_and_password(demo_email, demo_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), demo_password)
         .await
         .unwrap();
 
@@ -41,10 +36,6 @@ async fn sign_in_with_password_test_invalid() {
 
     let demo_email = "invalid@demo.com";
     let demo_password = "invalid";
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
 
     let session = auth_client
         .sign_in_with_email_and_password(demo_email, demo_password)
@@ -59,10 +50,6 @@ async fn sign_in_with_phone_and_password_test() {
 
     let demo_phone = "1234123412";
     let demo_password = "qwerqwer";
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
 
     let session = auth_client
         .sign_in_with_phone_and_password(demo_phone, demo_password)
@@ -81,12 +68,8 @@ async fn sign_up_with_email_test_valid() {
     let demo_email = format!("signup__{}@demo.com", uuid);
     let demo_password = "ciJUAojfZZYKfCxkiUWH";
 
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
-
     let session = auth_client
-        .sign_up_with_email_and_password(demo_email.clone(), demo_password.to_string())
+        .sign_up_with_email_and_password(demo_email.as_ref(), demo_password)
         .await
         .unwrap();
 
@@ -99,10 +82,6 @@ async fn sign_up_with_phone_test_valid() {
 
     let demo_phone = "13334445555";
     let demo_password = "ciJUAojfZZYKfCxkiUWH";
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
 
     let session = auth_client
         .sign_up_with_phone_and_password(demo_phone, demo_password)
@@ -119,16 +98,28 @@ async fn sign_up_with_phone_test_valid() {
 async fn send_login_email_with_magic_link() {
     let auth_client = create_test_client();
 
-    let demo_email = "demo@demo.com";
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
 
     let _response = auth_client
         .send_login_email_with_magic_link(demo_email)
-        .await
-        .unwrap();
+        .await;
+
+    assert!(_response.is_ok())
+}
+
+#[tokio::test]
+async fn send_email_with_otp() {
+    let auth_client = create_test_client();
+
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
+
+    let response = auth_client.send_email_with_otp(demo_email, None).await;
+
+    if response.is_err() {
+        eprintln!("{:?}", response.as_ref().unwrap_err())
+    }
+
+    assert!(response.is_ok())
 }
 
 #[tokio::test]
@@ -136,10 +127,6 @@ async fn send_sms_with_otp() {
     let auth_client = create_test_client();
 
     let demo_phone = "1333444555";
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
 
     let response = auth_client.send_sms_with_otp(demo_phone).await;
 
@@ -155,24 +142,16 @@ async fn sign_in_with_oauth_test() {
     let auth_client = create_test_client();
 
     // Must login to get a user bearer token
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
     let demo_password = "qwerqwer";
 
     let session = auth_client
-        .sign_in_with_email_and_password(demo_email, demo_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), demo_password)
         .await;
 
     if session.is_err() {
         eprintln!("{:?}", session.as_ref().unwrap_err())
     }
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
-    headers.insert(
-        header::AUTHORIZATION,
-        auth_client.api_key().parse().unwrap(),
-    );
 
     let mut params = HashMap::new();
     params.insert("key".to_string(), "value".to_string());
@@ -201,24 +180,16 @@ async fn sign_in_with_oauth_no_options_test() {
     let auth_client = create_test_client();
 
     // Must login to get a user bearer token
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
     let demo_password = "qwerqwer";
 
     let session = auth_client
-        .sign_in_with_email_and_password(demo_email, demo_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), demo_password)
         .await;
 
     if session.is_err() {
         eprintln!("{:?}", session.as_ref().unwrap_err())
     }
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
-    headers.insert(
-        header::AUTHORIZATION,
-        auth_client.api_key().parse().unwrap(),
-    );
 
     let response = auth_client
         .sign_in_with_oauth(supabase_auth::models::Provider::Github, None)
@@ -241,11 +212,11 @@ async fn get_user_test() {
     let auth_client = create_test_client();
 
     // Must login to get a user bearer token
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
     let demo_password = "qwerqwer";
 
     let session = auth_client
-        .sign_in_with_email_and_password(demo_email, demo_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), demo_password)
         .await;
 
     if session.is_err() {
@@ -265,11 +236,11 @@ async fn update_user_test() {
     let auth_client = create_test_client();
 
     // Must login to get a user bearer token
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
     let demo_password = "qwerqwer";
 
     let session = auth_client
-        .sign_in_with_email_and_password(demo_email, demo_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), demo_password)
         .await;
 
     if session.is_err() {
@@ -294,7 +265,7 @@ async fn update_user_test() {
     let test_password = "qqqqwwww";
 
     let new_session = auth_client
-        .sign_in_with_email_and_password(demo_email, test_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), test_password)
         .await;
 
     if new_session.is_err() {
@@ -319,15 +290,11 @@ async fn update_user_test() {
 async fn exchange_token_for_session() {
     let auth_client = create_test_client();
 
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
     let demo_password = "qwerqwer";
 
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
-
     let original_session = auth_client
-        .sign_in_with_email_and_password(demo_email, demo_password)
+        .sign_in_with_email_and_password(demo_email.as_ref(), demo_password)
         .await
         .unwrap();
 
@@ -349,7 +316,7 @@ async fn exchange_token_for_session() {
 async fn reset_password_for_email_test() {
     let auth_client = create_test_client();
 
-    let demo_email = "demo@demo.com";
+    let demo_email = env::var("DEMO_EMAIL").unwrap();
 
     let response = auth_client.reset_password_for_email(demo_email).await;
 
@@ -365,12 +332,7 @@ async fn resend_email_test() {
     let demo_email = format!("signup__{}@demo.com", uuid);
     let demo_password = "ciJUAojfZZYKfCxkiUWH";
 
-    let mut headers = header::HeaderMap::new();
-    headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("apikey", auth_client.api_key().parse().unwrap());
-
     let session = auth_client
-        // BUG: We need to run this without it instantly verifying
         .sign_up_with_email_and_password(demo_email.clone(), demo_password.to_string())
         .await;
 
