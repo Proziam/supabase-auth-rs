6efb253019cf35dba3ab90aa072b7068e9458e6f
Eric Biggs <eric@proziam.com>
2024-09-29 17:40:01 -0400

    Added Supabase HTTP Error

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,9 @@
-use std::env;
+use std::{
+    env,
+    fmt::{self, Display},
+};
 
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -26,4 +30,45 @@ pub enum Error {
     InvalidHeaderValue(#[from] reqwest::header::InvalidHeaderValue),
     #[error("Environment Variable Unreadable")]
     InvalidEnvironmentVariable(#[from] env::VarError),
+    #[error("{0}")]
+    Supabase(SupabaseHTTPError),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SupabaseHTTPError {
+    pub code: i32,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error_code: Option<String>,
+    #[serde(rename = "msg")]
+    pub message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub internal_error: Option<serde_json::Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub internal_message: Option<serde_json::Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub error_id: Option<String>,
+}
+
+impl Display for SupabaseHTTPError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Status Code {}", self.code)?;
+
+        if let Some(error_code) = &self.error_code {
+            write!(f, " ({})", error_code)?;
+        }
+
+        if let Some(error_id) = &self.error_id {
+            write!(f, " [Error ID: {}]", error_id)?;
+        }
+
+        if let Some(internal_message) = &self.internal_message {
+            write!(f, "\nInternal message: {}", internal_message)?;
+        }
+
+        if let Some(internal_error) = &self.internal_error {
+            write!(f, "\nInternal error: {}", internal_error)?;
+        }
+
+        write!(f, "\nMessage: {}", self.message)
+    }
 }
